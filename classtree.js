36ca3c385da54e9438b9a5bc34216edc64038505
Timo <situx@users.noreply.github.com>
2025-02-22 17:01:10 +0100

    Update classtree.js

--- classtree.js
+++ classtree.js
@@ -67,6 +67,7 @@ function generateClassTree(titleatt, superatt, classOrProp) {
                 console.log($(this).parent().parent().children("table"))
                 console.log($(this).parent().parent().children("table").children("tbody").children("tr").children("th"))
                 console.log($(this).parent().parent().children("table").children("tbody").children("tr").children("th").next().children("a"))
+                console.log($(this).parent().parent().children("table").children("tbody").children("tr").children("th").next().children("a")[0].attr("href"))
                 theth=$(this).parent().parent().children("table").children("tbody").children("tr").children("th")[2]
                 console.log(theth)
                 console.log($(theth).next())
